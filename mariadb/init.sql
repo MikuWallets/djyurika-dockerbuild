884f4b71d6ef5646a68f376ac01f8f4fe9163c1c
Seungwan Goh <emakr518@gmail.com>
2021-01-03 23:02:17 +0900

    Apply new table columns (v1.2.0)

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -9,5 +9,11 @@ flush privileges;
 DROP TABLE IF EXISTS `playlist`;
 
 CREATE TABLE `playlist` (
-  `id` varchar(50) NOT NULL
+  `id` varchar(50) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `reviewed` BIT NOT NULL,
+  `play_count` INT NOT NULL,
+  `pick_count` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `last_played_at` DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
